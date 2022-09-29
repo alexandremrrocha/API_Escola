eea63780f206499e16b8360de504815a0b6c19cd
Alexandre Miranda Rezende Rocha <alexandremrr2014@gmail.com>
2022-09-29 14:49:25 -0300

    FEAT|UPDATE: Criando os modelos e as migrações das tabelas Niveis, Turmas e Matriculas

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -4,6 +4,8 @@ const { Model} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Pessoas extends Model {
     static associate(models) {
+      Pessoas.hasMany(models.Turmas,{foreignKey: 'docente_id'})
+      Pessoas.hasMany(models.Matriculas,{foreignKey: 'estudante_id'})
     }
   }
   Pessoas.init({
