ee96043e729356cd5d1f75423657295b7ba56762
Alexandre Miranda Rezende Rocha <alexandremrr2014@gmail.com>
2022-09-30 15:17:04 -0300

    FEAT|UPDATE: Criação de metodos e rotas CRUD para a tabela matriculas

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -6,8 +6,11 @@ const router = Router();
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
 router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
 router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula)
-router.post('/pessoas',PessoaController.criaPessoa)
+router.post('/pessoas', PessoaController.criaPessoa)
+router.post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
 router.patch('/pessoas/:id', PessoaController.atualizaPessoa)
-router.delete('/pessoas/:id',PessoaController.deletaPessoa)
+router.patch('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula)
+router.delete('/pessoas/:id', PessoaController.deletaPessoa)
+router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.deletaMatricula)
 
 module.exports = router;
