a7d88e2d25000d346b3f2f47737da6d5fdd01257
Alexandre Miranda Rezende Rocha <alexandremrr2014@gmail.com>
2022-10-17 10:54:57 -0300

    FEAT: Validando dados de pessoas

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -9,9 +9,24 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Pessoas.init({
-    nome: DataTypes.STRING,
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        funcaoValidadora: function(dado) {
+          if(dado.length < 3) throw new Error('O campo nome deve ter mais de 3 caracteres')
+        }
+      }
+    },
     ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'Dados do tipo email esta inválido'
+        }
+      }
+    },
     role: DataTypes.STRING
   }, {
     sequelize,
